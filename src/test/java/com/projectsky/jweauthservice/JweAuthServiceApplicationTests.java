3a160ad7fe236f3508cb193bc179a68c5e4f2064
projectsky5 <mackvej@gmail.com>
2025-07-25 11:33:41 +0300

    Initial commit

---
+++ src/test/java/com/projectsky/jweauthservice/JweAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsky.jweauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JweAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
